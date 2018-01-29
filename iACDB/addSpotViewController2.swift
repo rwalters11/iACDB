7b4b6b8b5a77b30044db21a7a3693c3c46c273d9
rwalters11 <rwalters.flyer@icloud.com>
2018-01-29 21:27:48 +0000

    Eureka forms branched.

---
+++ iACDB/addSpotViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  addSpotViewController2.swift
+//  iACDB
+//
+//  Created by Richard Walters on 29/01/2018.
+//  Copyright © 2018 Richard Walters. All rights reserved.
+//
+
+import UIKit
+
+class addSpotViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
